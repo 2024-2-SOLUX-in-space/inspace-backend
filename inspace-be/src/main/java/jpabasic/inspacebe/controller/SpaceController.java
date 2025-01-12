81ef0b916d68b9d812d6b06cd0b3f013bca2921a
1224kang <cherry1224@sookmyung.ac.kr>
2025-01-12 21:51:18 +0900

    [FEAT] 공간 추가/조회/수정/삭제 기능 완료

--- inspace-be/src/main/java/jpabasic/inspacebe/controller/SpaceController.java
+++ inspace-be/src/main/java/jpabasic/inspacebe/controller/SpaceController.java
@@ -3,11 +3,14 @@ package jpabasic.inspacebe.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jpabasic.inspacebe.entity.Space;
 import jpabasic.inspacebe.service.SpaceService;
 import jpabasic.inspacebe.dto.SpaceDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -21,32 +24,32 @@ public class SpaceController {
         this.spaceService = spaceService;
     }
 
-    
+
     @PostMapping("/spaces/{user_id}")
     @Operation(summary="공간 신규 등록")
-    public ResponseEntity<SpaceDto> createSpace(@Valid @RequestBody SpaceDto spaceDto, @PathVariable int user_id){
+    public ResponseEntity<SpaceDto> createSpace(@Valid @RequestBody SpaceDto spaceDto, @PathVariable Integer user_id){
         spaceService.createSpace(spaceDto,user_id);
         return new ResponseEntity<>(spaceDto, HttpStatus.CREATED);
     }
 
-//    @GetMapping("/spaces/{user_id}")
-//    @Operation(summary="공간 목록 조회")
-//    public ResponseEntity<List<SpaceDto>> getSpaces(@PathVariable int user_id){
-//        List<SpaceDto> spaces=spaceService.getSpaces(user_id);
-//        return new ResponseEntity<>(spaces, HttpStatus.OK);
-//    }
+    @GetMapping("/spaces/{user_id}")
+    @Operation(summary="공간 목록 조회")
+    public ResponseEntity<List<SpaceDto>> getSpaces(@PathVariable Integer user_id){
+        List<SpaceDto> spaces=spaceService.getSpaces(user_id);
+        return new ResponseEntity<>(spaces, HttpStatus.OK);
+    }
 
     //예외처리해야.
-    @GetMapping("/spaces/{space_id}")
+    @GetMapping("/space/{space_id}")
     @Operation(summary="특정 공간 조회")
-    public ResponseEntity<SpaceDto> getSpace(@PathVariable int space_id){
+    public ResponseEntity<SpaceDto> getSpace(@PathVariable Integer space_id){
         SpaceDto space=spaceService.getSpace(space_id);
         return new ResponseEntity<>(space, HttpStatus.OK);
     }
 
     @PutMapping("/spaces/{space_id}/{user_id}") //user_id통해 본인만 설정 변경할 수 있도록.
     @Operation(summary="공간 설정 변경")
-    public ResponseEntity<SpaceDto> updateSpace(@PathVariable int space_id, @Valid @RequestBody SpaceDto spaceDto){
+    public ResponseEntity<SpaceDto> updateSpace(@PathVariable Integer space_id,@PathVariable Integer user_id, @Valid @RequestBody SpaceDto spaceDto){
         SpaceDto space= spaceService.updateSpace(space_id,spaceDto).getBody();
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
--- inspace-be/src/main/java/jpabasic/inspacebe/service/SpaceService.java
+++ inspace-be/src/main/java/jpabasic/inspacebe/service/SpaceService.java
@@ -2,43 +2,74 @@ package jpabasic.inspacebe.service;
 
 import jpabasic.inspacebe.dto.SpaceDto;
 import jpabasic.inspacebe.entity.Space;
+import jpabasic.inspacebe.entity.User;
 import jpabasic.inspacebe.repository.SpaceRepository;
+import jpabasic.inspacebe.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SpaceService {
 
     private final SpaceRepository spaceRepository;
+    private final UserRepository userRepository;
 
-    public SpaceService(SpaceRepository spaceRepository) {
+    public SpaceService(SpaceRepository spaceRepository, UserRepository userRepository) {
         this.spaceRepository = spaceRepository;
+        this.userRepository = userRepository;
     }
 
-    //새로운 공간 생성 //대표 공간 설정 로직 추가해야
-    public SpaceDto createSpace(SpaceDto dto, int userId){
-        Space space=Space.toEntity(dto);
-        //1. userId->userRepository에서 해당 유저의 공간들 리스트로 뽑는다
-        //2. 리스트에 공간 자체가 없을 때
-        //3. 대표 공간으로 설정
+    //새로운 공간 생성 
+    public SpaceDto createSpace(SpaceDto dto, Integer userId) {
 
 
-        //space.setUserId(userId);
-        spaceRepository.save(space);
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User Not Found"));
 
-        return SpaceDto.toDto(space);
+        //유저의 첫 공간 일 때, 해당 공간을 대표 공간으로 설정.\
+        //유저가 직접 공간을 '대표공간'이라고 설정 -> 원래 있던 대표 공간을 false로 바꾸기
+        List<Space> spaces = user.getSpaces();
+
+        if (spaces.isEmpty()) {
+            dto.setIsPublic(true);
+        } else {
+            for (Space target : spaces) {
+                if (target.getIsPublic().equals(true)) {
+                    target.setIsPublic(false);
+                    System.out.println("다른거 false 처리하고 true처리함");
+                    break;
+                }
+            }
+            dto.setIsPublic(true);
+        }
+
+
+            Space space = Space.toEntity(dto);
+            space.setUser(user);
+            spaceRepository.save(space);
+
+            return SpaceDto.toDto(space);
+
     }
 
     //공간 목록 조회
-//    public List<SpaceDto> getSpaces(int userId){
-//        //1. userId->userRepository에서 해당 유저의 공간들 리스트로 뽑는다
-//
-//    }
+    public List<SpaceDto> getSpaces(Integer userId){
+        //1. userId->userRepository에서 해당 유저의 공간들 리스트로 뽑는다
+        User user=userRepository.findById(userId)
+                .orElseThrow(()-> new RuntimeException("User Not Found"));
+        List<Space> spaces=user.getSpaces();
+        List<SpaceDto> spaceDtos=spaces.stream().map(SpaceDto::toDto).collect(Collectors.toList());
+
+        return spaceDtos;
+    }
 
     //특정 공간 조회 //타인이 조회 -> 조회할 수 없도록. //본인이 조회 -> 상관X
-    public SpaceDto getSpace(int spaceId){
-            Space space=spaceRepository.findById(spaceId)
-                    .orElseThrow(()->new IllegalArgumentException("존재하지 않는 공간입니다."));
+    public SpaceDto getSpace(Integer spaceId){
+        Space space=spaceRepository.findById(spaceId)
+                .orElseThrow(()->new IllegalArgumentException("존재하지 않는 공간입니다."));
         SpaceDto responseDto=SpaceDto.toDto(space);
         return responseDto;
     }
@@ -61,7 +92,7 @@ public class SpaceService {
         dto=SpaceDto.toDto(space);
         return ResponseEntity.ok(dto);
     }
-    
+
     //공간 삭제
     public void deleteSpace(int spaceId){
 
@@ -73,6 +104,6 @@ public class SpaceService {
         }catch (Exception e){
             throw new RuntimeException("삭제 하는 데 실패했습니다.");
         }
-        
+
     }
 }
