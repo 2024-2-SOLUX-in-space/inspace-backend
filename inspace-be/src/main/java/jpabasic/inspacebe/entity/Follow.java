86cf4db3b48ddb02d2e3ad6f6e845bf45b3f2bd2
Y00NSJ <ysj0509@sookmyung.ac.kr>
2025-01-17 13:24:37 +0900

    Rename: Follow 엔티티 필드명 수정
    - 객체에 대한 변수명이므로,
      '팔로우하는 사람'을 following으로,
      '팔로우 당하는 사람'을 followed로 수정

--- inspace-be/src/main/java/jpabasic/inspacebe/entity/Follow.java
+++ inspace-be/src/main/java/jpabasic/inspacebe/entity/Follow.java
@@ -2,20 +2,27 @@ package jpabasic.inspacebe.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Entity
 @Data
 public class Follow {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "follow_id")
-    private long id;
+    private Long id;
 
     @ManyToOne
     @JoinColumn(name = "follower_id")
-    private User followerId;
+    private User following; // 팔로우하는 사람의 아이디
 
     @ManyToOne
     @JoinColumn(name = "following_id")
-    private User followingId;
+    private User followed; // 팔로우 당하는 사람의 아이디
+
+    public Follow(User following, User followed) {
+        this.following = following;
+        this.followed = followed;
+    }
 }
