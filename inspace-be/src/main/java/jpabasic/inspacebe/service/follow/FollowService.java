680a1d5fca065e83aed12daf1ecfeff69e222d35
Y00NSJ <ysj0509@sookmyung.ac.kr>
2025-01-18 12:50:58 +0900

    Feat: 팔로잉 목록 열람, 팔로워 목록 열람 기능 구현
    - FollowRepository 에 findAllByFollowing(/Followeds) 구현
    - stream으로 직렬화, map으로 dto 통해 응답 객체 생성, collect으로 List화시켜 반환

--- inspace-be/src/main/java/jpabasic/inspacebe/service/follow/FollowService.java
+++ inspace-be/src/main/java/jpabasic/inspacebe/service/follow/FollowService.java
@@ -1,12 +1,18 @@
 package jpabasic.inspacebe.service.follow;
 
+import jpabasic.inspacebe.dto.follow.FollowResponseDto;
 import jpabasic.inspacebe.entity.Follow;
 import jpabasic.inspacebe.entity.User;
 import jpabasic.inspacebe.repository.FollowRepository;
 import jpabasic.inspacebe.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class FollowService {
@@ -19,6 +25,7 @@ public class FollowService {
         if (followRepository.existsByFollowingAndFollowed(following, followed)) {
             return "이미 팔로우 중입니다.";
         }
+
         followRepository.save(new Follow(followed, following));
         return followed.getName() + "님을 팔로우하였습니다.";
     }
@@ -32,4 +39,27 @@ public class FollowService {
         followRepository.delete(new Follow(followed, following));
         return followed.getName() + "님을 언팔로우하였습니다.";
     }
+
+    // "내가 팔로우하는" 사용자 목록
+    public List<FollowResponseDto> getFollowings(User following) {
+        List<Follow> followings = followRepository.findAllByFollowing(following);
+
+        return followings.stream()
+                .map(follow -> {
+                    return FollowResponseDto.fromEntity(follow.getFollowed());
+                })
+                .collect(Collectors.toList());
+    }
+
+    // "나를 팔로우하는" 사용자 목록
+    public List<FollowResponseDto> getFollowers(User followed) {
+        List<Follow> followers = followRepository.findAllByFollowed(followed);
+
+        return followers.stream()
+                .map(follow -> {
+                    return FollowResponseDto.fromEntity(follow.getFollowing());
+                })
+                .collect(Collectors.toList());
+    }
+
 }
