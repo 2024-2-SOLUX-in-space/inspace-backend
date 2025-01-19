3ce9f3dfb7d1de14ee2e43699c416cf63a55ca5e
1224kang <cherry1224@sookmyung.ac.kr>
2025-01-19 17:13:49 +0900

    [FEAT] 파이어베이스 설정 추가

--- inspace-be/src/main/java/jpabasic/inspacebe/config/FirebaseConfig.java
+++ inspace-be/src/main/java/jpabasic/inspacebe/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
@@ -13,14 +14,22 @@ import java.io.InputStream;
 /// https://console.firebase.google.com/project/inspace-d2239/storage/inspace-d2239.firebasestorage.app/files?hl=ko
 @Configuration
 public class FirebaseConfig {
+
+    @Value("${firebase.service-account-key}")
+    private String serviceAccountKey;
+
+    @Value("${firebase.storage-bucket}")
+    private String storageBucket;
+
+
     @PostConstruct
     public void initializeFirebase() throws IOException {
         FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/inspace-d2239-firebase-adminsdk-fbsvc-7962a1caf6.json");
+                new FileInputStream(serviceAccountKey);
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setStorageBucket("inspace-d2239.firebasestorage.app") // Cloud Storage 버킷 URL
+                .setStorageBucket(storageBucket) // Cloud Storage 버킷 URL
                 .build();
 
         FirebaseApp.initializeApp(options);
